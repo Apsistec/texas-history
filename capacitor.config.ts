2af821880257cae47cac10715d6dbcc0ea13487c
Douglas White <email@douglaswhite.dev>
2024-08-30 01:35:36 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'texas-history',
+  webDir: 'www'
+};
+
+export default config;
